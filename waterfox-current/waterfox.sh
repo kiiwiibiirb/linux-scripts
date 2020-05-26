98fd5c4891abbc0cd7300926b559bd6eaf1cb17b
Sam Corbitt <samcorbitt_@outlook.com>
2020-05-26 09:53:21 -0500

    Fixed waterfox.sh

--- waterfox-current/waterfox.sh
+++ waterfox-current/waterfox.sh
@@ -2,6 +2,10 @@
 cd ~/Downloads/
 wget https://cdn.waterfox.net/releases/linux64/installer/waterfox-current-2020.05.en-US.linux-x86_64.tar.bz2 -O waterfox.tar.bz2
 tar xvf waterfox.tar.bz2
+rm ~/Downloads/waterfox.tar.bz2
 sudo mv ~/Downloads/waterfox /opt/
 sudo wget https://raw.githubusercontent.com/MrAlex94/Waterfox/classic/browser/branding/unofficial/default128.png -O /opt/waterfox/waterfox.png
-sudo wget 
+sudo wget https://github.com/kiiwiibiirb/linux-scripts/raw/master/waterfox-current/waterfox-current.desktop -O /usr/share/applications/waterfox-current.desktop
+sudo chmod +x /usr/share/applications/waterfox-current.desktop
+clear
+echo "Waterfox is now installed. have fun!"
